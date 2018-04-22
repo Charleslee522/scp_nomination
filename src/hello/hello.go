63a84d0d8c151f15e7dc78a8c055733e05b94dc0
Charles <charleslee522@gmail.com>
2018-04-22 09:37:14 +0900

    add stringutil pkg

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Charleslee522/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
