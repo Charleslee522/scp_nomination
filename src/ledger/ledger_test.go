9b03e50eabe9862b68af25d1b74372630101c649
Charles <charleslee522@gmail.com>
2018-04-22 17:04:11 +0900

    add value pool and message struct

--- src/ledger/ledger_test.go
+++ src/ledger/ledger_test.go
@@ -8,92 +8,94 @@ import (
 type Node struct {
 	name     string
 	priority int
-	// message  []string
 }
 
 func (n Node) GetName() string {
 	return n.name
 }
 
-type Message struct {
+type Value struct {
 	data string
 }
 
-type Ledger struct {
-	node       Node
-	validators []Node
+type SCPNomination struct {
+	votes    []Value
+	accepted []Value
+	nodeName string
 }
 
-func (l Ledger) receiveMessage(msg Message) {
+// func newSCPNomination(data string, node Node) Value {
+// 	m := SCPNomination{data, node.GetName()}
+// 	return m
+// }
+
+type Ledger struct {
+	node        Node
+	validators  []Node
+	n_validator uint32
+}
+
+func (l Ledger) receiveMessage(msg SCPNomination) {
 	fmt.Println(l.node.GetName())
 	fmt.Println(msg)
 }
 
+type FederatedVotingState int
+
+const (
+	Votes FederatedVotingState = 1 + iota
+	Accepted
+)
+
+func (l Ledger) getValueState(value *Value) FederatedVotingState {
+	return Accepted
+}
+
 // 노드 세 개 정의(n1, n2, n3)
 // 이 테스트의 주인공은 n1
-// 서로를 모두 validator 로 지정
+// n2와 n3을 validator 로 지정
 // 리더 선출(n1) - TestLedgerSelfLeader
 // 	다른 노드의 메시지를 받으면 저장만 하고, echoing 하지 않음
 // 리더 선출(n2) - TestLedgerOtherLeader
 // 	n2의 메시지를 받으면 저장 후에 echoing 함
 // 	n3의 메시지를 받으면 저장만 하고, echoing 하지 않음
 
-func TestLedger(t *testing.T) {
-
-	var node1 Node = Node{"n1", 1}
-	var node2 Node = Node{"n2", 0}
-	var node3 Node = Node{"n3", 0}
+func TestLedgerSelfLeader(t *testing.T) {
+	var node1 Node = Node{"n1", 3}
+	var node2 Node = Node{"n2", 2}
+	var node3 Node = Node{"n3", 1}
 
 	nodes := []Node{node1, node2, node3}
 
-	var ledger1 Ledger = Ledger{Node{"n1", 1}, nodes}
-	var ledger2 Ledger = Ledger{Node{"n2", 0}, nodes}
-	var ledger3 Ledger = Ledger{Node{"n3", 0}, nodes}
+	v11 := Value{"value11"}
+	v12 := Value{"value12"}
+	// v21 := Value{"value21"}
+	// v22 := Value{"value22"}
+	// v31 := Value{"value31"}
+	// v32 := Value{"value32"}
 
-	fmt.Println(ledger1.validators)
+	var ledger1 Ledger = Ledger{node1, nodes, 3}
+	// var ledger2 Ledger = Ledger{node2, nodes, 3}
+	// var ledger3 Ledger = Ledger{node3, nodes, 3}
+	vPool1 := []Value{v11, v12}
+	// vPool2 := []Value{v21, v22}
+	// vPool3 := []Value{v31, v32}
 
-	var msg1 Message = Message{"I'm Charles!"}
+	msgFrom2 := SCPNomination{}
+	msgFrom2.votes = append(msgFrom2.votes, vPool1[0])
+	msgFrom2.votes = append(msgFrom2.votes, vPool1[1])
+	msgFrom2.nodeName = node2.GetName()
 
-	ledger1.receiveMessage(msg1)
-	ledger2.receiveMessage(msg1)
-	ledger3.receiveMessage(msg1)
+	msgFrom3 := SCPNomination{}
+	msgFrom3.votes = append(msgFrom3.votes, vPool1[0])
+	msgFrom3.votes = append(msgFrom3.votes, vPool1[1])
+	msgFrom3.nodeName = node3.GetName()
 
-	// bc1 = blockchain_factory(
-	//     node_name_1,
-	//     'http://localhost:5001',
-	//     100,
-	//     [node_name_2, node_name_3]
-	// )
+	ledger1.receiveMessage(msgFrom2)
+	ledger1.receiveMessage(msgFrom3)
 
-	// bc2 = blockchain_factory(
-	//     node_name_2,
-	//     'http://localhost:5002',
-	//     100,
-	//     [node_name_1, node_name_3]
-	// )
+	if ledger1.getValueState(&v11) != Accepted {
+		t.Errorf("v11 State == %q, want %q", ledger1.getValueState(&v11), Accepted)
+	}
 
-	// bc3 = blockchain_factory(
-	//     node_name_3,
-	//     'http://localhost:5003',
-	//     100,
-	//     [node_name_1, node_name_2]
-	// )
-
-	// bc1.consensus.add_to_validator_connected(bc2.node)
-	// bc1.consensus.add_to_validator_connected(bc3.node)
-	// bc1.consensus.init()
-
-	// message = Message.new('message')
-	// ballot_init_1 = Ballot.new(node_name_1, message, IsaacState.INIT, BallotVotingResult.agree)
-	// ballot_id = ballot_init_1.ballot_id
-	// ballot_init_2 = Ballot(ballot_id, node_name_2, message, IsaacState.INIT, BallotVotingResult.agree,
-	//                        ballot_init_1.timestamp)
-	// ballot_init_3 = Ballot(ballot_id, node_name_3, message, IsaacState.INIT, BallotVotingResult.agree,
-	//                        ballot_init_1.timestamp)
-
-	// bc1.receive_ballot(ballot_init_1)
-	// bc1.receive_ballot(ballot_init_2)
-	// bc1.receive_ballot(ballot_init_3)
-
-	// assert bc1.consensus.slot.get_ballot_state(ballot_init_1) == IsaacState.SIGN
 }
