248527863a5db15d5d9cf5bad409510952c6bdbb
Charles <charleslee522@gmail.com>
2018-04-24 15:58:30 +0900

    make nodes and ledgers in run-ledger module

--- src/run/run-ledger.go
+++ src/run/run-ledger.go
@@ -7,16 +7,14 @@ import (
 	"log"
 	filepath "path/filepath"
 
+	. "github.com/Charleslee522/scp_nomination/src/common"
+	. "github.com/Charleslee522/scp_nomination/src/ledger"
 	yaml "gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Node map[string]Node
-}
-
-type Node struct {
-	Port       int
-	Validators []string
+	Default map[string]int
+	Node    map[string]Node
 }
 
 func (c *Config) getConf() *Config {
@@ -38,9 +36,32 @@ func (c *Config) getConf() *Config {
 	return c
 }
 
+func GetNodeSlice(nodesMap map[string]Node, validators []string) []Node {
+	nodes := []Node{}
+	for _, name := range validators {
+		// if nodesMap[name] == nil {
+		// 	continue
+		// }
+		nodes = append(nodes, nodesMap[name])
+	}
+	return nodes
+}
+
 func main() {
 	var c Config
 	c.getConf()
 
 	fmt.Println(c)
+
+	threshold := c.Default["threshold"]
+	nodes := make(map[string]Node)
+	for _, v := range c.Node {
+		nodes[v.Name] = v
+	}
+
+	for _, v := range c.Node {
+		ledger := NewLedger(v, GetNodeSlice(nodes, v.Validators), threshold)
+		fmt.Println(ledger)
+	}
+
 }
